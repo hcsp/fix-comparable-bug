e147e2f7190e10685d6f223f177ae439618a70e7
zhangyongwang <20225614@qq.com>
2019-07-02 23:04:26 +0800

    修复Comparable实现中的bug

--- src/test/java/com/github/hcsp/polymorphism/UserTest.java
+++ src/test/java/com/github/hcsp/polymorphism/UserTest.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
