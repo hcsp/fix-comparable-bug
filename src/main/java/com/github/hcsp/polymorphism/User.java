0d6412b90e55a58094780c0c14f6366881c72592
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2019-12-16 09:16:22 +0800

    CompareTo2

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -47,11 +47,12 @@ public class User implements Comparable<User> {
     /** 老板说让我按照用户名排序 */
     @Override
     public int compareTo(User o) {
-        if(!id.equals(o.id)) {
-            return id.compareTo(o.id);
-        }else {
-            return name.compareTo(o.name);
-        }
+       int flag = name.compareTo(o.name);
+        System.out.println(flag);
+       if(flag == 0 ) {
+           return id.compareTo(o.id);
+       }
+       return flag;
 
     }
 
